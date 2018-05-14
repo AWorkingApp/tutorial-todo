c8f18150a65e53b1933558065f9cd6398b62ee77
chen <tianclch@gmail.com>
2018-05-14 00:37:36 -0400

    implement TodoInput component

--- src/App.js
+++ src/App.js
@@ -7,13 +7,28 @@ import TodoDetail from './components/TodoDetail';
 import './App.css';
 
 class App extends PureComponent {
+  /**
+   * todo : {
+   *   id: currentTime
+   *   complete: bool,
+   *   content: string,
+   * }
+   */
+  state = {
+    todos: [],
+  }
+
   render() {
     return (
       <div className='container'>
         <div style={{ height: '100%', width: 400 }}>
           <TodoTitle />
           <div style={{ margin: 25, marginTop: 16, }}>
-            <TodoInput />
+            <TodoInput addTodo={(todo) => {
+              this.setState((prevState) => ({
+                todos: [{ id: Date.now(), complete: false, content: todo }].concat(prevState.todos)
+              }));
+            }} />
           </div>
           <div style={{ marginTop: 10 }}>
             <TodoDetail />
--- src/components/TodoInput/index.js
+++ src/components/TodoInput/index.js
@@ -9,8 +9,37 @@ export default class TodoInput extends React.PureComponent {
         addTodo: PropTypes.func,
     }
 
+    static defaultProps = {
+        addTodo: (input) => { },
+    }
+
+    state = {
+        inputValue: '',
+    }
+
+    addTodo = () => {
+        this.props.addTodo(this.state.inputValue);
+        this.setState({ inputValue: '' });
+    }
+
+    _allowAdd(value) {
+        return value !== undefined && value.length > 0;
+    }
+
     render() {
+        const { inputValue } = this.state;
+
         return <div className="todo-input-container shadow-container" >
+            <TextInput
+                value={inputValue}
+                onChange={(value) => {
+                    this.setState({inputValue: value});
+                }} 
+                addTodo={this.addTodo}
+            />
+            {
+                this._allowAdd(inputValue) ? <AddTodo addTodo={this.addTodo} /> : null
+            }
         </div>;
     }
 };
@@ -19,10 +48,32 @@ const TextInput = (props) => {
     return <input
         className="todo-input"
         type="text"
-        placeholder="Enter TODO here." />
+        placeholder="Enter TODO here."
+        value={props.value}
+        onChange={(e) => {
+            props.onChange(e.target.value);
+        }}
+        onKeyPress={(e) => {
+            if (e.key === 'Enter') {
+                props.addTodo();
+            }
+        }}
+    />
+}
+
+TextInput.propTypes = {
+    addTodo: PropTypes.func,
+    onChange: PropTypes.func,
 }
 
 const AddTodo = (props) => {
-    return <span className="add-todo">
+    return <span className="add-todo" onClick={() => {
+        props.addTodo();
+    }}>
+        {`Add`}
     </span>;
+}
+
+AddTodo.propTypes = {
+    addTodo: PropTypes.func,
 }
