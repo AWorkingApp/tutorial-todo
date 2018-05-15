b3db71cd7dced4ffa882963b6e94d97578551418
chen <tianclch@gmail.com>
2018-05-15 00:48:46 -0400

    Add Filter Header

--- src/components/TodoDetail/index.js
+++ src/components/TodoDetail/index.js
@@ -3,6 +3,10 @@ import PropTypes from 'prop-types';
 
 import './todoList.css';
 
+const FilterAll = 0;
+const FilterTodo = 1;
+const FilterCompleted = 2;
+
 export default class TodoDetail extends React.PureComponent { 
     static propTypes = {
         todos: PropTypes.array,
@@ -15,11 +19,54 @@ export default class TodoDetail extends React.PureComponent {
         onRemove: (id) => {},
     }
 
+    state = {
+        filterValue: FilterAll,
+        filterItems: [
+            {
+                name: 'All',
+                value: FilterAll,
+            },
+            {
+                name: 'To Do',
+                value: FilterTodo,
+            },
+            {
+                name: 'Completed',
+                value: FilterCompleted,
+            },
+        ]
+    }
+
+    _activeTodoCount(todos) {
+        return todos.filter((todo) => !todo.complete).length;
+    }
+
+    _filteredTodos(todos, filterValue) {
+        switch (filterValue) {
+            case FilterCompleted:
+                return todos.filter((todo) => todo.complete);
+
+            case FilterTodo:
+                return todos.filter((todo) => !todo.complete);
+
+            case FilterAll:
+            default:
+                return todos;
+        }
+    }
+
     render() {
         const { todos, onComplete, onRemove } = this.props;
         return <div className='todo-detail-container shadow-container'>
+             <TodoHeader
+                filterItems={this.state.filterItems}
+                activeCount={this._activeTodoCount(todos)}
+                onFilter={(filterValue) => {
+                    this.setState({ filterValue: filterValue });
+                }}
+            />
             <TodoList
-                todos={todos}
+                todos={this._filteredTodos(todos, this.state.filterValue)}
                 onComplete={onComplete}
                 onRemove={onRemove}
             />
@@ -27,25 +74,44 @@ export default class TodoDetail extends React.PureComponent {
     }
 }
 
+
 function TodoHeader(props) {
     return <div className='todo-header flex-center'>
             <div className='todo-header-count'> 
+                {`${props.activeCount} left`}
             </div>
             <div className='todo-header-filter-container flex-center'>
+                <TodoFilter filterItems={props.filterItems} onFilter={props.onFilter} />
             </div>
         </div>
 }
 
 class TodoFilter extends React.PureComponent {
+
+    state = {
+        activeFilterIndex: 0,
+    }
+
     render() {
         return <div className='todo-filter-container'>
+            {
+                this.props.filterItems.map((item, index) => <TodoFilterItem 
+                        key={index}
+                        active={index === this.state.activeFilterIndex} 
+                        onFilter={(value) => {
+                            this.setState({ activeFilterIndex: index });
+                            this.props.onFilter(value);
+                        }}
+                        {...item} 
+                    />)
+            }
         </div>
     }
 }
 
 function TodoFilterItem (props) {
     let className = props.active ? 'filter-item-active' : 'filter-item-inactive'
-    return (<div className={`${className} filter-item`}>
+    return (<div className={`${className} filter-item`} onClick={() => props.onFilter(props.value)}>
         <span>{props.name}</span>
     </div>)
 }
